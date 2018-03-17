f3403e454e041831e65c218571d26406ee858621
Lauchi <simonheiss87@gmail.com>
2018-03-17 18:16:49 +0100

    Add project files.

---
+++ Host/ResetDb.ps1
@@ -0,0 +1,6 @@
+﻿Remove-Item -Force GeneratedWebService/Eventstore.db
+cd SqlAdapter/
+dotnet ef migrations remove -s ../GeneratedWebService/
+dotnet ef migrations add InitialMigration -s ../GeneratedWebService/
+dotnet ef database update -s ../GeneratedWebService/
+cd ..
---
+++ Host/Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Host
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //GeneratedDependencies.ConfigureGeneratedServices(services);
+            //services.AddDbContext<EventStoreContext>(option => option.UseSqlite("Data Source=Eventstore.db"));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseMvc();
+        }
+    }
+}
